76a83211d0c3dfedf8d61b74c695550597473da0
Wilson Wu <iwilsonwu@gmail.com>
2020-02-12 23:03:48 +0800

    clean code

--- src/main/java/codingkids/mc/creativetabs/CreativeTabs.java
+++ src/main/java/codingkids/mc/creativetabs/CreativeTabs.java
@@ -1,7 +1,10 @@
 package codingkids.mc.creativetabs;
 
 import codingkids.mc.creativetabs.proxy.CommonProxy;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -19,14 +22,19 @@ public class CreativeTabs {
 
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
-
-    public static Item.ToolMaterial myTool;
     public static Item mySword;
-
+    public static final net.minecraft.creativetab.CreativeTabs DEMO_CREATIVETABS = new net.minecraft.creativetab.CreativeTabs(0, "ckcreativetabs.demo_creativetabs") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(Items.DIAMOND_SWORD);
+        }
+    };
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        myTool = EnumHelper.addToolMaterial("demo_sword", 4, 1000, 100F, 10000F, 100);
-        mySword = new MySword(myTool);
+        mySword = new ItemSword(EnumHelper.addToolMaterial("demo_sword", 4, 1000, 100F, 50000F, 100));
+        mySword.setRegistryName("demo_sword");
+        mySword.setTranslationKey("ckcreativetabs.demo_sword");
+        mySword.setCreativeTab(DEMO_CREATIVETABS);
     }
 
     @EventHandler
