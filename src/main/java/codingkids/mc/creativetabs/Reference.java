a2987c1024656556459f2824a92152b4e5c4d284
Wilson Wu <iwilsonwu@gmail.com>
2020-02-12 13:59:07 +0800

    rename modid

--- src/main/java/codingkids/mc/creativetabs/Reference.java
+++ src/main/java/codingkids/mc/creativetabs/Reference.java
@@ -5,7 +5,7 @@ package codingkids.mc.creativetabs;
  */
 public class Reference
 {
-    public static final String MODID = "creativetabs";
+    public static final String MODID = "ckcreativetabs";
     public static final String NAME = "demo_creativetabs";
     public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSION = "[1.12.2]";
